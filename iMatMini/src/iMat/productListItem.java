da2af2c730b28d103a936886243157430197e767
rapunschel <rapunschel.gaming@gmail.com>
2022-05-14 18:17:30 +0200

    Added Constructor / ecological image if product is eco

--- iMatMini/src/iMat/productListItem.java
+++ iMatMini/src/iMat/productListItem.java
@@ -2,15 +2,23 @@ package iMat;
 
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.Product;
 
-public class productListItem {
+public class ProductListItem {
 
+    private IMatDataHandlerWrapper parentController;
+    private Product product;
     @FXML
     private ImageView productImageView;
+
+    @FXML
+    private ImageView ecoFriendlyImageView;
 
     @FXML
     private Label productNameLabel;
@@ -31,44 +39,33 @@ public class productListItem {
     private AnchorPane addProductAnchorPane;
 
 
-    public productListItem() {
-        this.productImageView = productImageView;
-        this.productNameLabel = productNameLabel;
-        this.priceLabel = priceLabel;
-        this.otherInfoLabel = otherInfoLabel;
+    public ProductListItem(Product product, IMatDataHandlerWrapper controller) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("productItem.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        // Add information about product.
+        this.parentController = controller;
+
+        this.productNameLabel.setText(product.getName());
+
+        this.priceLabel.setText(product.getPrice() + " " + product.getUnitSuffix());
+
+        this.productImageView.setImage(controller.getFXImage(product,
+                                                             productImageView.getFitWidth(),
+                                                             productImageView.getFitHeight()));
+
+
+        if (product.isEcological()) {
+            Image ecoImage = new Image("iMat/resources/ecological_icon");
+            this.ecoFriendlyImageView.setImage(ecoImage);
+            this.otherInfoLabel.setText("Denna vara är ekologisk.\n" + "ProduktID: " + product.getProductId());
+        } else {
+            this.otherInfoLabel.setText("ProduktID: " + product.getProductId());
+        }
+
     }
 
-    public void setProductImageView(ImageView productImageView) {
-        this.productImageView = productImageView;
-    }
-
-    public void setProductNameLabel(Label productNameLabel) {
-        this.productNameLabel = productNameLabel;
-    }
-
-    public void setPriceLabel(Label priceLabel) {
-        this.priceLabel = priceLabel;
-    }
-
-    public void setOtherInfoLabel(Label otherInfoLabel) {
-        this.otherInfoLabel = otherInfoLabel;
-    }
-
-    public ImageView getProductImageView() {
-        return productImageView;
-    }
-
-    public Label getProductNameLabel() {
-        return productNameLabel;
-    }
-
-    public Label getPriceLabel() {
-        return priceLabel;
-    }
-
-    public Label getOtherInfoLabel() {
-        return otherInfoLabel;
-    }
 
     public AnchorPane getAddRemoveProductAnchorPane() {
         return addRemoveProductAnchorPane;
@@ -76,5 +73,9 @@ public class productListItem {
 
     public AnchorPane getAddProductAnchorPane() {
         return addProductAnchorPane;
+    }
+
+    public Product getProduct() {
+        return product;
     }
 }
