45e65504810f765e4539abe02e9375f500427945
rapunschel <rapunschel.gaming@gmail.com>
2022-05-14 19:10:02 +0200

    Added FXML variables to controller

--- iMatMini/src/iMat/IMatController.java
+++ iMatMini/src/iMat/IMatController.java
@@ -2,15 +2,43 @@ package iMat;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.Product;
+
+import javax.swing.text.html.ImageView;
 
 public class IMatController implements Initializable {
+
+    private final IMatDataHandlerWrapper  controller = IMatDataHandlerWrapper.getInstance();
+
+    // Needed for listeners when clicking on product buttons
+    @FXML
+    private Button addProductButton;
+
+    @FXML
+    private AnchorPane addRemoveProductAnchorPane; // -[int] +
+
+    @FXML
+    private AnchorPane addProductAnchorPane;     // To switch which anchorPane to show
+
+    @FXML
+    private ImageView starImageView;
+
+    @FXML
+    private TextField productCounterTextField;
+
+    private final Map<String, Product> productItemMap = new HashMap<>();
+
 
     //new
     @FXML
