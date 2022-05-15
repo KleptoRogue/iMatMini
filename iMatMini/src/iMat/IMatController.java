cba0846009e28c71400fa8c49144334a534d88b8
rapunschel <rapunschel.gaming@gmail.com>
2022-05-15 14:36:33 +0200

    Added some pliancy to add button

--- iMatMini/src/iMat/IMatController.java
+++ iMatMini/src/iMat/IMatController.java
@@ -11,8 +11,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import se.chalmers.cse.dat216.project.Product;
@@ -21,17 +23,12 @@ import javax.swing.text.html.ImageView;
 
 public class IMatController implements Initializable {
 
-    private final IMatDataHandlerWrapper backend = IMatDataHandlerWrapper.getInstance();
+    private final IMatDataHandlerWrapper model = IMatDataHandlerWrapper.getInstance();
 
     // Needed for listeners when clicking on product buttons
     @FXML
     private Button addProductButton;
 
-    @FXML
-    private AnchorPane addRemoveProductAnchorPane; // -[int] +
-
-    @FXML
-    private AnchorPane addProductAnchorPane;     // To switch which anchorPane to show
 
     @FXML
     private ImageView starImageView;
@@ -59,14 +56,28 @@ public class IMatController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        updateProductList(backend.getProducts());
+        updateProductList(model.getProducts());
     }
 
     private void updateProductList(List<Product> products) {
-
         productFlowPane.getChildren().clear();
         for (Product product : products) {
-            productFlowPane.getChildren().add(new ProductListItem(product));
+            productFlowPane.getChildren().add(new ProductListItem(product, this));
         }
+    }
+
+
+    protected void addProduct(Product product, Node anchorpane) {
+        //TODO Connect to backend to update cart and productCounterTextField
+        anchorpane.toFront();
+    }
+
+    protected void removeProduct(Product product, Node anchorpane) {
+        // TODO Connect to backend to update cart and productCounterTextField, switch anchorpane if 0
+        anchorpane.toFront();
+    }
+
+    protected Image getFXImage(Product product, double width, double height) {
+        return model.getFXImage(product, width, height);
     }
 }
--- iMatMini/src/iMat/productListItem.java
+++ iMatMini/src/iMat/productListItem.java
@@ -1,10 +1,10 @@
 package iMat;
 
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import se.chalmers.cse.dat216.project.Product;
@@ -13,7 +13,7 @@ import java.io.IOException;
 
 public class ProductListItem extends AnchorPane {
 
-    private final IMatDataHandlerWrapper backend = IMatDataHandlerWrapper.getInstance();
+    private IMatController parentcontroller;
     private Product product;
     @FXML
     private ImageView productImageView;
@@ -28,18 +28,23 @@ public class ProductListItem extends AnchorPane {
     private Label priceLabel;
 
     @FXML
-    private Label otherInfoLabel;
+    private Label ecoLabel;
+
+    @FXML
+    private AnchorPane addRemoveProductAnchorPane;
+
+    @FXML
+    private AnchorPane addProductAnchorPane;
+
+    // Count products added to cart
+    private int productCounter = 0;
 
 
-
-
-    public ProductListItem(Product product) {
+    public ProductListItem(Product product, IMatController controller) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("productItem.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
 
-        // Add information about product.
-        this.product = product;
 
         try {
             fxmlLoader.load();
@@ -47,29 +52,48 @@ public class ProductListItem extends AnchorPane {
             throw new RuntimeException(exception);
         }
 
+        // Add information about product.
+        this.product = product;
+        this.parentcontroller = controller;
+
         productNameLabel.setText(product.getName());
 
         priceLabel.setText(product.getPrice() + " kr / " + product.getUnitSuffix());
 
-        productImageView.setImage(backend.getFXImage(product,
+        productImageView.setImage(parentcontroller.getFXImage(product,
                                                              productImageView.getFitWidth(),
                                                              productImageView.getFitHeight()));
 
 
         if (product.isEcological()) {
-         //   Image ecoImage = new Image("imat/resources/ecological_icon");
-           // ecoFriendlyImageView.setImage(ecoImage);
-            otherInfoLabel.setText("Denna vara är ekologisk.\n" + "ProduktID: " + product.getProductId());
-        } else {
-            otherInfoLabel.setText("ProduktID: " + product.getProductId());
+          //  Image ecoImage = new Image("ecological_icon.png");
+       //     ecoFriendlyImageView.setImage(image);
+            ecoLabel.setText("Ekologisk");
         }
 
     }
 
 
 
+    @FXML
+    public void onAddClickEvent(Event event) {
+        parentcontroller.addProduct(product, addRemoveProductAnchorPane);
+    }
 
+    @FXML
+    public void onRemoveClickEvent(Event event) {
+        parentcontroller.removeProduct(product, addProductAnchorPane);
+    }
     public Product getProduct() {
         return product;
+    }
+
+
+    public int getProductCounter() {
+        return productCounter;
+    }
+
+    public void setProductCounter(int productCounter) {
+        this.productCounter = productCounter;
     }
 }
