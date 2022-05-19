5586fcc03265499a842d608a55fc6ead7cb9a182
IsakGMagnusson <isak.g.magnusson@gmail.com>
2022-05-19 09:29:56 +0200

    Fixed errors

--- iMatMini/src/iMat/IMatController.java
+++ iMatMini/src/iMat/IMatController.java
@@ -37,8 +37,6 @@ public class IMatController implements Initializable {
     @FXML
     private AnchorPane mainpage;
 
-    @FXML private AnchorPane cartAnchorPane;
-    @FXML private AnchorPane loginAnchorPane;
 
     //För klick på loggin icon
     public void logginClicked(){
@@ -46,9 +44,11 @@ public class IMatController implements Initializable {
     }
 
     //För klick på cart icon
-    public void cartClicked(){
-        // TODO skriv in FXML fil namn för att kunna klicka på icon
-        //NAMEHERE.toFront();
+    public void cartClicked() throws IOException{
+        loaderPane.getChildren().clear();;
+
+        AnchorPane newLoadedPane =  FXMLLoader.load(getClass().getResource("FXML/cart.fxml"));
+        loaderPane.getChildren().add(newLoadedPane);
     }
 
     @FXML
@@ -72,55 +72,43 @@ public class IMatController implements Initializable {
     }
 
     @FXML
-    public void loginToFront(){
-        loginpage.toFront();
-    }
-
-    @FXML
     public void mainToFront(){
         mainpage.toFront();
     }
 
     @FXML
-    public void loadWizard (ActionEvent event) throws IOException {
-        loaderPane.getChildren().remove(loaderPane.getChildren());
+    public void loadWizard () throws IOException {
+        loaderPane.getChildren().clear();;
         mainToFront();
         AnchorPane newLoadedPane =  FXMLLoader.load(getClass().getResource("FXML/RegisterWizard.fxml"));
         loaderPane.getChildren().add(newLoadedPane);
     }
 
     @FXML
-    public void loadStart (ActionEvent event) throws IOException {
-        loaderPane.getChildren().remove(loaderPane.getChildren());
+    public void loadStart () throws IOException {
+        loaderPane.getChildren().clear();;
 
         AnchorPane newLoadedPane =  FXMLLoader.load(getClass().getResource("FXML/StartPage.fxml"));
         loaderPane.getChildren().add(newLoadedPane);
     }
 
     @FXML
-    public void loadAccount (ActionEvent event) throws IOException {
-        loaderPane.getChildren().remove(loaderPane.getChildren());
+    public void loadAccount () throws IOException {
+        loaderPane.getChildren().clear();;
 
         AnchorPane newLoadedPane =  FXMLLoader.load(getClass().getResource("FXML/acount.fxml"));
-        loaderPane.getChildren().add(newLoadedPane);
-    }
-
-    @FXML
-    public void loadCart (ActionEvent event) throws IOException {
-        loaderPane.getChildren().remove(loaderPane.getChildren());
-
-        AnchorPane newLoadedPane =  FXMLLoader.load(getClass().getResource("FXML/cart.fxml"));
         loaderPane.getChildren().add(newLoadedPane);
     }
 
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
        updateProductList(model.getProducts());
     }
 
     private void updateProductList(List<Product> products) {
-        productFlowPane.getChildren().clear();
+      //  productFlowPane.getChildren().clear();
         for (Product product : products) {
             productFlowPane.getChildren().add(new ProductItem(product, this));
         }
--- iMatMini/src/iMat/iMat.java
+++ iMatMini/src/iMat/iMat.java
@@ -11,7 +11,7 @@ public class iMat extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXML/iMatMini.fxml"));
+        Parent root = FXMLLoader. load(getClass().getResource("FXML/acount.fxml"));
         Scene scene = new Scene(root);
         stage.setResizable(false);
         stage.setScene(scene);
