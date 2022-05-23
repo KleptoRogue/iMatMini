d5ee43de81fc1c7e4e79341fb23b1e53dfdd7f95
rapunschel <rapunschel.gaming@gmail.com>
2022-05-23 19:07:22 +0200

    Made productItem.java adds shoppingItem object to cart + can write in textfield to change amount"

--- iMatMini/src/iMat/ProductItem.java
+++ iMatMini/src/iMat/ProductItem.java
@@ -1,6 +1,8 @@
 package iMat;
 
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,8 +11,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import se.chalmers.cse.dat216.project.Product;
+import se.chalmers.cse.dat216.project.ShoppingCart;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ProductItem extends AnchorPane {
 
@@ -50,16 +55,34 @@ public class ProductItem extends AnchorPane {
     @FXML
     private TextField productCounterTextField;
 
+    ShoppingItem shoppingItem;
+
+
     // Count products added to cart
     private int productCounter = 0;
 
 
     public ProductItem(Product product, IMatController controller) {
+        initializeFXML();
+        this.product = product;
+        this.parentcontroller = controller;
+        initializeNewShoppingItem(product);
+        initializeProductInformation();
+        initializeProductCounterListener();
+    }
 
+    public ProductItem(Product product, IMatController controller, ShoppingItem item) {
+
+    }
+
+    private void initializeNewShoppingItem(Product product) {
+        this.shoppingItem = new ShoppingItem(product);
+        this.shoppingItem.setAmount(0);
+    }
+    private void initializeFXML() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FXML/ProductItem.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
-
 
 
         try {
@@ -67,32 +90,28 @@ public class ProductItem extends AnchorPane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
-
-        // Add information about product.
-        this.product = product;
-        this.parentcontroller = controller;
-
+    }
+    private void initializeProductInformation() {
         productNameLabel.setText(product.getName());
 
         priceLabel.setText(product.getPrice() + " kr / " + product.getUnitSuffix());
 
         productImageView.setImage(model.getFXImage(product,
-                                                             productImageView.getFitWidth(),
-                                                             productImageView.getFitHeight()));
+                productImageView.getFitWidth(),
+                productImageView.getFitHeight()));
 
 
         if (model.isFavorite(this.product)) {
             favoritedAnchorPane.toFront();
         }
 
-
         if (product.isEcological()) {
             //Image ecoImage = new Image("ecological_icon.png");
             //ecoFriendlyImageView.setImage(ecoImage);
             ecoLabel.setText("Ekologisk");
         }
-    }
 
+    }
     @FXML
     public void addFavoriteEvent(Event event) {
         model.addFavorite(product);
@@ -110,24 +129,76 @@ public class ProductItem extends AnchorPane {
     @FXML
     public void addProductClickEvent(Event event) {
         //TODO Connect to backend to update cart
-        addRemoveProductAnchorPane.toFront();
+
+        ShoppingCart cart = model.getShoppingCart();
+
+        if (shoppingItem.getAmount() == 0) {
+            addRemoveProductAnchorPane.toFront();
+            cart.addItem(shoppingItem);
+        }
+
+        shoppingItem.setAmount(shoppingItem.getAmount() + 1);
+        updateCounterTextField();
+        System.out.println("product: " + shoppingItem.getProduct());
+        System.out.println("Amount: " + shoppingItem.getAmount());
+
     }
 
     @FXML
     public void removeProductClickEvent(Event event) {
         // TODO Connect to backend to update cart
-        addProductAnchorPane.toFront();
-    }
-    public Product getProduct() {
-        return product;
+        ShoppingCart cart = model.getShoppingCart();
+        shoppingItem.setAmount(shoppingItem.getAmount() - 1);
+
+        if (shoppingItem.getAmount() == 0) {
+            addProductAnchorPane.toFront();
+            cart.removeItem(this.shoppingItem);
+            System.out.println("Cart: " + cart.getItems());
+
+        }
+        updateCounterTextField();
+        System.out.println("product: " + shoppingItem.getProduct());
+        System.out.println("Amount: " + shoppingItem.getAmount());
     }
 
+    public void initializeProductCounterListener() {
+        productCounterTextField.focusedProperty().addListener(new ChangeListener<Boolean>() {
 
-    public int getProductCounter() {
-        return productCounter;
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+
+                if(newValue){
+                    //focusgained - do nothing
+                }
+                else{
+                    System.out.println(productCounterTextField.getText());
+                    try {
+                        int intValue = Integer.parseInt(productCounterTextField.getText());
+                        if (intValue < 0) {
+                            updateCounterTextField(); // Revert to old value.
+                        } else {
+                            if (intValue == 0) {
+                                addProductAnchorPane.toFront();
+                                shoppingItem.setAmount(0);
+                                model.getShoppingCart().removeItem(shoppingItem);
+                                System.out.println(model.getShoppingCart().getItems());
+                            } else {
+                                shoppingItem.setAmount(intValue);
+                                updateCounterTextField();
+                                System.out.println(shoppingItem.getAmount());
+                            }
+
+                        }
+
+                    } catch (NumberFormatException error) {
+                        updateCounterTextField(); // Revert to old value
+                    }
+                }
+            }
+        });
     }
-
-    public void setProductCounter(int productCounter) {
-        this.productCounter = productCounter;
+    private void updateCounterTextField() {
+        productCounterTextField.clear();
+        productCounterTextField.setText( ((int) shoppingItem.getAmount()) + "");
     }
 }
